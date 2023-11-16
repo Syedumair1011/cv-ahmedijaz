0fe5cbb155b457f1b0d342ce3d302550861be323
Syed Umair Husnain <syedumairhusnain@outlook.com>
2023-11-16 16:29:24 +0400

    CV Image Change
    Center the image in mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -19,21 +19,21 @@ function App() {
         <div className='row'>
           <div className='col-lg-8'>
           <div className='row'>
-             <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12'>
-             <img src={cvlogo} style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover', objectPosition: 'center top', }} alt="CV Logo" />
-              </div>
-              <div className='col-lg-5 col-md-5 col-sm-12 col-xs-12'>
-                <h2>
+          <div className='col-lg-3 col-md-3 col-sm-12 col-xs-12' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <img src={cvlogo} style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover', objectPosition: 'center top' }} alt="CV Logo" />
+            </div>
+              <div className='col-lg-5 col-md-5 col-sm-12 col-xs-12' >
+                <h2 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                   Ahmed Ijaz Khawaja
                 </h2>
-                <h5 style={{fontWeight: 'bolder'}}>Innovative | Creative | Energetic</h5>
-                <h6>Incharge Support Center</h6>
+                <h5 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontWeight: 'bolder' }}>Innovative | Creative | Energetic</h5>
+                <h6 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Incharge Support Center</h6>
               </div>
               </div>
           </div>
-          <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12'>
+          <div className='col-lg-4 col-md-4 col-sm-12 col-xs-12' >
             <div className='row align-items-center active'>
-              <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
+              <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2' >
               <svg xmlns='http://www.w3.org/2000/svg' x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
                 <path d="M 14 3.9902344 C 8.4886661 3.9902344 4 8.4789008 4 13.990234 L 4 35.990234 C 4 41.501568 8.4886661 45.990234 14 45.990234 L 36 45.990234 C 41.511334 45.990234 46 41.501568 46 35.990234 L 46 13.990234 C 46 8.4789008 41.511334 3.9902344 36 3.9902344 L 14 3.9902344 z M 18.005859 12.033203 C 18.633859 12.060203 19.210594 12.414031 19.558594 12.957031 C 19.954594 13.575031 20.569141 14.534156 21.369141 15.785156 C 22.099141 16.926156 22.150047 18.399844 21.498047 19.589844 L 20.033203 21.673828 C 19.637203 22.237828 19.558219 22.959703 19.824219 23.595703 C 20.238219 24.585703 21.040797 26.107203 22.466797 27.533203 C 23.892797 28.959203 25.414297 29.761781 26.404297 30.175781 C 27.040297 30.441781 27.762172 30.362797 28.326172 29.966797 L 30.410156 28.501953 C 31.600156 27.849953 33.073844 27.901859 34.214844 28.630859 C 35.465844 29.430859 36.424969 30.045406 37.042969 30.441406 C 37.585969 30.789406 37.939797 31.366141 37.966797 31.994141 C 38.120797 35.558141 35.359641 37.001953 34.556641 37.001953 C 34.000641 37.001953 27.316344 37.761656 19.777344 30.222656 C 12.238344 22.683656 12.998047 15.999359 12.998047 15.443359 C 12.998047 14.640359 14.441859 11.879203 18.005859 12.033203 z"></path>
               </svg>
@@ -89,41 +89,24 @@ function App() {
           <tbody>
           <tr>
             <td>
-            <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
+
               <h5>Hajvery University, Lahore</h5>
               <h6 style={{textAlign: 'left', fontWeight: 'bolder'}}>Bachelors</h6>
               <h6>September 2010 - AUGUST 2014</h6>
               <h6>Degree: BSCS (Bachelors in Computer Science)</h6>
               <h6>CGPA: 3.0</h6>
-          </div>
-        </div>
-      </div>
             </td>
             <td>
-            <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
               <h5>FG Degree College Kharian Cantr</h5>
               <h6 style={{textAlign: 'left', fontWeight: 'bolder'}}>Intermediate</h6>
               <h6>May 2009</h6>
               <h6>Grade: D</h6>
-          </div>
-        </div>
-      </div>
             </td>
             <td>
-            <div className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
               <h5>Cantonment Public School Kharian Cantt</h5>
               <h6 style={{textAlign: 'left', fontWeight: 'bolder'}}>Matriculation</h6>
               <h6>March 2005</h6>
               <h6>Grade: B</h6>
-          </div>
-        </div>
-      </div>
             </td>
           </tr>
           </tbody>
